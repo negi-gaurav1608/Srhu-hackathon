571e2e5b05d8fae29e20a8e4128a8e8d4210be66
Gaurav <ngaurav825@gmail.com>
2024-05-14 23:01:34 +0530

    hackathon

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-neutral-950 text-white w-full'>
+        Hello everyone
+      </div>
+      
+    </>
+  )
+}
+
+export default App
